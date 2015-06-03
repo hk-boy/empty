212b05ff7cb4b01978c0da66b1f697c5609466b2
jiuqi <yumiao@iMac.local>
2015-06-03 15:18:26 +0800

    The  first

--- empty/ViewController.m
+++ empty/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *num = @"100";
+    NSLog(@"%@",num);
 }
 
 - (void)didReceiveMemoryWarning {
