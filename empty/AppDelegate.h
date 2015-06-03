f4c58483e09a97db002bf5e0217db459600f8d9c
jiuqi <yumiao@iMac.local>
2015-06-03 15:09:08 +0800

    initial commit

---
+++ empty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  empty
+//
+//  Created by jiuqi on 15/6/3.
+//  Copyright (c) 2015年 jiuqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
